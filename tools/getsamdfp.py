6273050d78127111a12d008185684c88b05e3ead
IOL0ol1 <IOL0ol1@outlook.com>
2024-05-11 01:24:50 +0800

    update

--- tools/getsamdfp.py
+++ tools/getsamdfp.py
@@ -1,51 +1,53 @@
+ï»¿from glob import iglob
+import json
 import requests
 import os
 import shutil
 import zipfile
-import argparse
 import re
+import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 def parse_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     
-    # ÕÒµ½ËùÓĞ id Îª accordion µÄ div ÔªËØ
+    # æ‰¾åˆ°æ‰€æœ‰ id ä¸º accordion çš„ div å…ƒç´ 
     accordion_divs = soup.find_all('div', {'id': 'accordion'})
     
     result = []
     
-    # ±éÀúÃ¿¸ö accordion_div
+    # éå†æ¯ä¸ª accordion_div
     for accordion_div in accordion_divs:
-        # ÕÒµ½µ±Ç° accordion_div ÏÂµÄËùÓĞ×Ó div ÔªËØ
+        # æ‰¾åˆ°å½“å‰ accordion_div ä¸‹çš„æ‰€æœ‰å­ div å…ƒç´ 
         child_divs = accordion_div.findChildren('div', recursive=False)
         
-        # ±éÀúÃ¿¸ö×Ó div
+        # éå†æ¯ä¸ªå­ div
         for child_div in child_divs:
-            # ÕÒµ½º¬ÓĞ data-link ±êÇ©µÄµÚÒ»¸ö div ÔªËØ
+            # æ‰¾åˆ°å«æœ‰ data-link æ ‡ç­¾çš„ç¬¬ä¸€ä¸ª div å…ƒç´ 
             first_div_with_data_link = child_div.find(attrs={'data-link': True})
             
-            # Èç¹ûÕÒµ½ÁË·ûºÏÌõ¼şµÄ div£¬Ôò½«ÆäÄÚÈİÌí¼Óµ½½á¹ûÊı×éÖĞ
+            # å¦‚æœæ‰¾åˆ°äº†ç¬¦åˆæ¡ä»¶çš„ divï¼Œåˆ™å°†å…¶å†…å®¹æ·»åŠ åˆ°ç»“æœæ•°ç»„ä¸­
             if first_div_with_data_link:
                 result.append(first_div_with_data_link['data-link'])
     
     return result
 
 def clean_folders(base_folder):
-    # ±éÀúbase_folderÏÂµÄÃ¿¸ö×ÓÎÄ¼ş¼Ğ
+    # éå†base_folderä¸‹çš„æ¯ä¸ªå­æ–‡ä»¶å¤¹
     for root, dirs, files in os.walk(base_folder):
-        # ¼ì²éµ±Ç°ÎÄ¼ş¼ĞÖĞÊÇ·ñÓĞ'include'ÎÄ¼ş¼Ğ
+        # æ£€æŸ¥å½“å‰æ–‡ä»¶å¤¹ä¸­æ˜¯å¦æœ‰'include'æ–‡ä»¶å¤¹
         if 'include' in dirs: 
-            # ±éÀúµ±Ç°ÎÄ¼ş¼ĞÏÂµÄËùÓĞ×ÓÎÄ¼ş¼Ğ
+            # éå†å½“å‰æ–‡ä»¶å¤¹ä¸‹çš„æ‰€æœ‰å­æ–‡ä»¶å¤¹
             for dir in dirs:
                 if dir not in ['gcc', 'include']:
-                    # ¹¹ÔìÍêÕûµÄÎÄ¼ş¼ĞÂ·¾¶
+                    # æ„é€ å®Œæ•´çš„æ–‡ä»¶å¤¹è·¯å¾„
                     full_dir_path = os.path.join(root, dir)
-                    # É¾³ı²»ÊÇ'gcc'»ò'include'µÄÎÄ¼ş¼Ğ
+                    # åˆ é™¤ä¸æ˜¯'gcc'æˆ–'include'çš„æ–‡ä»¶å¤¹
                     shutil.rmtree(full_dir_path)
                     print(f"deleted folder: {full_dir_path}")
                     
 def download_and_extract(url, output_dir, cache='./cache'):
-    # ·¢ËÍÇëÇó
+    # å‘é€è¯·æ±‚
     response = requests.get(url)
     if response.status_code == 200: 
         
@@ -54,15 +56,15 @@ def download_and_extract(url, output_dir, cache='./cache'):
          
         os.makedirs(cache,exist_ok=True)
         os.makedirs(output_dir,exist_ok=True)
-        # ÏÂÔØ²¢½âÑ¹ÎÄ¼ş
+        # ä¸‹è½½å¹¶è§£å‹æ–‡ä»¶
         for link in data_links:
-            # ¹¹½¨ÏÂÔØÂ·¾¶
+            # æ„å»ºä¸‹è½½è·¯å¾„
             download_url = os.path.join(url, link)
             
-            # ÏÂÔØÎÄ¼şµ½ÁÙÊ±Ä¿Â¼
+            # ä¸‹è½½æ–‡ä»¶åˆ°ä¸´æ—¶ç›®å½•
             atpack_filename = os.path.basename(download_url)
             atpack_path = os.path.join(cache, atpack_filename)
-            # Èç¹ûÎÄ¼ş²»´æÔÚÔòÏÂÔØ
+            # å¦‚æœæ–‡ä»¶ä¸å­˜åœ¨åˆ™ä¸‹è½½
             if not os.path.exists(atpack_path):
                 with open(atpack_path, 'wb') as f:
                     f.write(requests.get(download_url).content)
@@ -70,7 +72,7 @@ def download_and_extract(url, output_dir, cache='./cache'):
             
             series = re.search(r'^Atmel\.(.*?)\.', atpack_filename).group(1)
             version = re.search(r"DFP\.(\d+\.\d+\.\d+)(?:\.atpack)?", atpack_filename).group(1)
-            # ½âÑ¹µ½Ö¸¶¨Ä¿Â¼µÄÍ¬ÃûÎÄ¼ş¼ĞÏÂ
+            # è§£å‹åˆ°æŒ‡å®šç›®å½•çš„åŒåæ–‡ä»¶å¤¹ä¸‹
             with zipfile.ZipFile(atpack_path, 'r') as zip_ref:
                 series_dir = os.path.join(output_dir, series, version)
                 os.makedirs(series_dir,exist_ok=True)
@@ -81,19 +83,69 @@ def generate_header(input_dir,output_dir):
     #clean_folders(input_dir)
     print(input_dir)
 
+def get_devices(pdsc_file):
+    devices = []
+    # æŸ¥æ‰¾devicesèŠ‚ç‚¹ä¸‹çš„familyèŠ‚ç‚¹
+    family_nodes = ET.parse(pdsc_file).getroot().findall('.//devices/family')
+    # è¾“å‡ºfamilyèŠ‚ç‚¹ä»¥åŠå…¶ä¸‹çš„deviceèŠ‚ç‚¹
+    for family_node in family_nodes: 
+        # è·å–familyèŠ‚ç‚¹ä¸‹çš„deviceèŠ‚ç‚¹
+        device_nodes = family_node.findall('device')
+        for device_node in device_nodes:
+            device = {}
+            device['series'] = family_node.get('Dfamily')
+            device['device'] = device_node.get('Dname')
+            device['core'] = device_node.find('processor').get('Dcore') 
+            device['endian'] = device_node.find('processor').get('Dendian') 
+            device['mpu'] = device_node.find('processor').get('Dmpu') 
+            device['fpu'] = device_node.find('processor').get('Dfpu') 
+            device['header'] = device_node.find('compile').get('header') 
+            device['define'] = device_node.find('compile').get('define')
+            
+            for memory_node in device_node.findall("./memory[@default]"):
+                if 'startup' in memory_node.attrib:
+                    flash_memory = memory_node
+                    device['romstart'] = flash_memory.get('start')
+                    device['romsize'] = flash_memory.get('size')
+                else:
+                    ram_memory = memory_node
+                    device['ramstart'] = ram_memory.get('start')
+                    device['ramsize'] = ram_memory.get('size')
+            atdf = device_node.find('.//at:atdf',{'at': 'http://www.atmel.com/schemas/pack-device-atmel-extension'}).get('name')
+            atdf_file = os.path.join(os.path.dirname(pdsc_file),atdf)
+            atdf_root = ET.parse(atdf_file).getroot();
+            device['family'] = atdf_root.find('.//devices/device').get('family')
+            device['defines'] = {}
+            for parame_node in atdf_root.find('.//devices/device/parameters').findall('param'):
+                device['defines'][parame_node.get('name')] = parame_node.get('value')
+            devices.append(device)
+    return devices
+ 
+def get_all_devices(folder):
+    devices = []
+    for file in  [file for file in iglob(os.path.join(folder, '**/*.pdsc'), recursive=True)]:
+        devices.extend(get_devices(file))
+    return devices
+    
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Download and extract files from a webpage.')
-    parser.add_argument('--url', help='URL of the webpage to scrape', default='http://packs.download.atmel.com/')
-    parser.add_argument('--output-dir', help='Output directory for extracted files', default='./Atmel')
-    parser.add_argument('--cache', help='Web download cache', default='./cache')
-    args = parser.parse_args()
     
-    # ¼ì²éÊä³öÄ¿Â¼ÊÇ·ñ´æÔÚ£¬Èç¹û²»´æÔÚÔò´´½¨
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
+  
+    a = get_all_devices('./temp')
+    with open("data.json", "w") as file:
+        file.write(json.dumps(a))
+    # parser = argparse.ArgumentParser(description='Download and extract files from a webpage.')
+    # parser.add_argument('--url', help='URL of the webpage to scrape', default='http://packs.download.atmel.com/')
+    # parser.add_argument('--output-dir', help='Output directory for extracted files', default='./Atmel')
+    # parser.add_argument('--cache', help='Web download cache', default='./cache')
+    # args = parser.parse_args()
     
-    temp_dir = "./temp"
-    download_and_extract(args.url, temp_dir)
-    generate_header(temp_dir, args.output_dir)
+    # # æ£€æŸ¥è¾“å‡ºç›®å½•æ˜¯å¦å­˜åœ¨ï¼Œå¦‚æœä¸å­˜åœ¨åˆ™åˆ›å»º
+    # if not os.path.exists(args.output_dir):
+    #     os.makedirs(args.output_dir)
+    
+    # temp_dir = "./temp"
+    # download_and_extract(args.url, temp_dir)
+    # generate_header(temp_dir, args.output_dir)
     #shutil.rmtree(temp_dir)
